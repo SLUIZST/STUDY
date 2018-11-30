0c2976b91f9e5f72361dc5739a1463689ee64cd0
= <=>
2018-11-29 22:07:51 -0200

    Case Study - Tweet Trump - Final

--- Data Wrangling/Wrangling.R
+++ Data Wrangling/Wrangling.R
@@ -114,6 +114,8 @@ converted <- s %>%
 
 
 
+library("stringr", lib.loc="~/R/win-library/3.4")
+
 # ================== String Processing Part 3 ===============
 
 # --- Using Groups and Quantifiers
@@ -208,6 +210,162 @@ extract(data = tab, col = x, into = c("feet", "inches", "decimal"),
 extract(data = tab, col = x, into = c("feet", "inches", "decimal"), 
         regex = "(\\d)'(\\d{1,2})(\\.\\d+)?")    
 
-# -----------------------
+# -------------------------------------------------------
+
+# ----- String Splitting
+
+
+# Question 1
+
+staff <- "Mandy, Chris and Laura"
+
+str_split(staff, ",|and")
+str_split(staff, ", | and ")         # OK    
+str_split(staff, ",\\s|\\sand\\s")   # OK
+str_split(staff, "\\s?(,|and)\\s?")
+
+# Question 2
+
+schedule <- data.frame(day=c("Monday", "Tuesday"), staff=c("Mandy, Chris and Laura", "Steve, Ruth and Frank"))
+
+library("tidyr", lib.loc="~/R/win-library/3.4")
+tidy <- schedule %>% mutate(staff = str_split(staff, ", | and ")) %>% unnest()  # OK
+
+tidy <- separate(schedule, staff, into = c("s1","s2","s3"), sep = ",") %>% 
+  gather(key = s, value = staff, s1:s3)
+
+tidy <- schedule %>% 
+  mutate(staff = str_split(staff, ", | and ", simplify = TRUE)) %>% 
+  unnest()
+
+
+# -------------------------------------------------------
+# --- Case Study: Extracting a Table from a PDF
+
+# One of the datasets provided in dslabs shows scientific funding rates by gender in the Netherlands:
+  
+library(dslabs)
+data("research_funding_rates")
+research_funding_rates 
+
+# Downloading the data
+
+library("pdftools")
+temp_file <- tempfile()
+url <- "http://www.pnas.org/content/suppl/2015/09/16/1510159112.DCSupplemental/pnas.201510159SI.pdf"
+download.file(url, temp_file)
+txt <- pdf_text(temp_file)
+file.remove(temp_file)
+
+raw_data_research_funding_rates <- txt[2]
+
+data("raw_data_research_funding_rates")
+
+# Looking at the download
+library("dplyr")
+raw_data_research_funding_rates %>% head
+
+# Each line on the page, including the table rows, is separated by the symbol for newline: \n.
+# We can therefore can create a list with the lines of the text as elements:
+
+library("stringr")  
+tab <- str_split(raw_data_research_funding_rates, "\n")
+
+# Because we start off with just one element in the string, we end up with a list with just one entry:
+tab <- tab[[1]]
+
+# By examining this object,
+tab %>% head
+
+# we see that the information for the column names is the third and forth entires:
+  
+the_names_1 <- tab[3]
+the_names_2 <- tab[4]
+
+# Extracting the table data
+the_names_1
+
+the_names_1 <- the_names_1 %>%
+  str_trim() %>%
+  str_replace_all(",\\s.", "") %>%
+  str_split("\\s{2,}", simplify = TRUE)
+the_names_1
+
+# Now let's look at the second line:
+
+the_names_2
+
+the_names_2 <- the_names_2 %>%
+  str_trim() %>%
+  str_split("\\s+", simplify = TRUE)
+the_names_2
+
+# Now we can join these to generate one name for each column:
+  
+tmp_names <- str_c(rep(the_names_1, each = 3), the_names_2[-1], sep = "_")
+the_names <- c(the_names_2[1], tmp_names) %>%
+  str_to_lower() %>%
+  str_replace_all("\\s", "_")
+the_names
+
+
+# Now we are ready to get the actual data. By examining the tab object, we notice 
+# that the information is in lines 6 through 14. We can use str_split again to 
+# achieve our goal:
+library("tidyverse")
+
+new_research_funding_rates <- tab[6:14] %>%
+  str_trim %>%
+  str_split("\\s{2,}", simplify = TRUE) %>%
+  data.frame(stringsAsFactors = FALSE) %>%
+  setNames(the_names) %>%
+  mutate_at(-1, parse_number)
+new_research_funding_rates %>% head()
+
+# We can see that the objects are identical:
+  
+identical(research_funding_rates, new_research_funding_rates)
+
+
+# -------------------------------------------------------
+# ---  Recoding
+
+# Question 1
+
+dat <- gapminder %>% filter(region == "Middle Africa") %>% 
+  mutate(country_short = recode(country, 
+                                "Central African Republic" = "CAR", 
+                                "Congo, Dem. Rep." = "DRC",
+                                "Equatorial Guinea" = "Eq. Guinea"))
+
+
+# ============ Dates, Times, and Text Mining  ======================
+
+# -- Text Mining
+
+# --- Case study: Trump Tweets
+
+library(tidyverse)
+library(ggplot2)
+library(lubridate)
+library(tidyr)
+library(scales)
+set.seed(1)
+
+# -- In general, we can extract data directly from twitter using the \emph{rtweet} package.
+# However, in this case, a group has already compiled data for us and made it available 
+# at http://www.trumptwitterarchive.com.
+
+url <- 'http://www.trumptwitterarchive.com/data/realdonaldtrump/%s.json'
+trump_tweets <- map(2009:2017, ~sprintf(url, .x)) %>%
+  map_df(jsonlite::fromJSON, simplifyDataFrame = TRUE) %>%
+  filter(!is_retweet & !str_detect(text, '^"')) %>%
+  mutate(created_at = parse_date_time(created_at, orders = "a b! d! H!:M!:S! z!* Y!", tz="EST")) 
+
+
+head(trump_tweets)
+
+names(trump_tweets)
+
 
 
